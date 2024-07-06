5f1117444f4e2fe5692e5e4134cd314eb3ed8a44
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-07-06 10:07:28 +1000

    Set up tailwind.config file and global.css file for the project

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,12 +7,68 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "custom-dark": "#030712",
+        "custom-light": "#ffffff",
+        "custom-primary": "#2892F9",
+        "cs-syd-1": "#8FD7ED",
+        "cs-syd-2": "#5ACAEC",
+        "cs-syd-3": "#28B8E4",
+        "cs-syd-4": "#22A3DC",
+        "cs-syd-5": "#3292CC",
+        "cs-syd-6": "#2D82BB",
+        "cs-syd-7": "#2B6F9B",
+        "cs-syd-8": "#26577C",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        carouselSlide: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        showProjectContent: {
+          "0%": {
+            transform: "translateY(50px)",
+            filter: "blur(30px)",
+            opacity: 0,
+          },
+          "100%": { transform: "translateY(0)", filter: "blur(0)", opacity: 1 },
+        },
+      },
+      animation: {
+        carouselSlide: "carouselSlide 30s infinite linear",
+        showProjectContentOrganisation:
+          "showProjectContent 0.5s ease-in-out 0.7s forwards",
+        showProjectContentTitle:
+          "showProjectContent 0.5s ease-in-out 0.9s forwards",
+        showProjectContentDescription:
+          "showProjectContent 0.5s ease-in-out 1.1s forwards",
+        showProjectContentButton:
+          "showProjectContent 0.5s ease-in-out 1.3s forwards",
+      },
+      gridTemplateColumns: {
+        "40-60": "40% 60%",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          ".text-outline": {
+            "-webkit-text-stroke-width": "1px",
+            "-webkit-text-stroke-color": "rgba(255,255,255, 0.3)",
+          },
+          ".text-outline-2": {
+            "-webkit-text-stroke-width": "2px",
+            "-webkit-text-stroke-color": "currentColor",
+          },
+        },
+        ["responsive", "hover"]
+      );
+    },
+  ],
+  darkMode: "class",
 };
