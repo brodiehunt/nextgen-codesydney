66e070a6b371cfa9b3878043490a62483aa23cd4
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-08-19 06:13:23 +1000

    Breaks when I use process.env within the createClient function?

--- client/src/utils/sanity.js
+++ client/src/utils/sanity.js
@@ -1,5 +1,7 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
+const PROJECT_ID = process.env.SANITY_PROJECT_ID;
+console.log(PROJECT_ID, "project_id");
 
 export const client = createClient({
   projectId: "8zqunlka", // replace with your actual project ID
