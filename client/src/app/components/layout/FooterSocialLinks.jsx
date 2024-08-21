b24e37872cb925358c6e16cc73124c1115d79c38
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-08-22 04:28:39 +1000

    Make footer social links acceptable by adding aria-label property and sr-only text component

--- client/src/app/components/layout/FooterSocialLinks.jsx
+++ client/src/app/components/layout/FooterSocialLinks.jsx
@@ -14,38 +14,50 @@ const socialLinksGroup1 = [
   {
     href: "https://discord.gg/yHNKkvMN5e",
     icon: FaDiscord,
+    label: "Go to Code.Sydney Discord",
   },
   {
     href: "https://www.meetup.com/Sydney-Volunteer-Programmers/",
     icon: FaMeetup,
+    label: "Go to Code.Sydney Meetup.com group",
   },
   {
     href: "https://www.linkedin.com/company/code.sydney/",
     icon: FaLinkedinIn,
+    label: "Go to Code.Sydney LinkedIn profile",
   },
   {
     href: "https://www.instagram.com/code.sydney/",
     icon: FaInstagram,
+    label: "Go to Code.Sydney Instagram profile",
   },
 ];
 const socialLinksGroup2 = [
   {
     href: "https://www.facebook.com/codesyd/",
     icon: FaFacebookF,
+    nlabel: "Go to Code.Sydney Facebook Profile",
   },
   {
     href: "https://twitter.com/code_sydney",
     icon: FaTwitter,
+    label: "Go to Code.Sydney Twitter Profile",
   },
   {
     href: "https://www.youtube.com/channel/UCnOp_l8-EX3c-q2Tlj-2Iyw",
     icon: FaYoutube,
+    label: "Go to Code.Sydney Youtube channel",
   },
   {
     href: "https://github.com/codesydney",
     icon: FaGithub,
+    label: "Go to Code.Sydney Github Profile",
   },
 ];
+
+const VisuallyHidden = ({ children }) => (
+  <span className="sr-only">{children}</span>
+);
 
 export default function FooterSocialLinks() {
   return (
@@ -61,8 +73,10 @@ export default function FooterSocialLinks() {
                   key={index}
                   className="group relative cursor-pointer block py-1 rounded-full text-custom-light px-2 lg:px-0 lg:pl-4"
                   href={link.href}
+                  aria-label={link.label}
                 >
                   <IconComponent className="text-3xl group-hover:text-custom-primary transition-all duration-200" />
+                  <VisuallyHidden>{link.label}</VisuallyHidden>
                 </a>
               );
             })}
@@ -76,8 +90,10 @@ export default function FooterSocialLinks() {
                   key={index}
                   className="group relative cursor-pointer block py-1 rounded-full text-custom-light px-2 lg:px-0 lg:pl-4"
                   href={link.href}
+                  aria-label={link.label}
                 >
                   <IconComponent className="text-3xl group-hover:text-custom-primary transition-all duration-200" />
+                  <VisuallyHidden>{link.label}</VisuallyHidden>
                 </a>
               );
             })}
