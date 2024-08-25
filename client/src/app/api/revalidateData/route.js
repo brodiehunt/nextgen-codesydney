12d38eef97afca30d226cec3b0c8c2bc6f1d98bd
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-08-26 05:45:54 +1000

    Revalidation strategy

--- client/src/app/api/revalidateData/route.js
+++ client/src/app/api/revalidateData/route.js
@@ -3,15 +3,17 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const body = await request.json();
-
+  console.log("Recieved webhook:", body);
   // Check for secret to confirm this is a valid request
   if (body.secret !== process.env.SANITY_WEBHOOK_SECRET) {
     return NextResponse.json({ message: "Invalid token" }, { status: 401 });
   }
-  console.log("Recieved webhook:", body);
+
   try {
     if (body._type === "homepage") {
+      console.log("Revalidating homepage tag");
       revalidateTag("homepage");
+      console.log("Revalidation completed");
       return NextResponse.json({
         revalidated: true,
         now: Date.now(),
@@ -27,6 +29,7 @@ export async function POST(request) {
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
+    console.error("Error during revalidation:", err);
     return NextResponse.json(
       { message: "Error revalidating" },
       { status: 500 }
